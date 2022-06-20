ed03717dadd8d06c3daedc3ae39e5d83fa7cc9a5
berson969 <berson969@PragueMac.local>
2022-06-20 08:14:56 +0200

    Commit after checking SQL requests homework

--- SQL requests.sql
+++ SQL requests.sql
@@ -5,11 +5,16 @@ SELECT name, duration FROM Tracks
 ORDER BY  duration DESC
 LIMIT 1;
 
+--Вариант с функцией MAX
+SELECT name, duration FROM Tracks
+WHERE duration = (SELECT MAX(duration) FROM Tracks);
+
 SELECT name, duration FROM Tracks
 WHERE  duration >= '3:30';
 
+--Заменил на BETWEEN
 SELECT name, released_year  FROM COllections
-WHERE released_year >= 2018 AND released_year <= 2020;
+WHERE released_year BETWEEN 2018 AND 2020;
 
 
 SELECT name FROM Actors
